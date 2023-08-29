f8da3bf098494796f9986a429ed7ae6d34d3948e
blacOOil <111561598+blacOOil@users.noreply.github.com>
2023-08-29 23:14:23 +0700

    Add ItemListandGridShowFeature

---
+++ Assets/Script/ItemArray.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inventory.ItemArray
+{
+    public class ItemArray : MonoBehaviour
+    { 
+        public List<Item> Items = new List<Item>();
+        [SerializeField]
+ 
+        void start()
+         {
+          
+         }
+      
+    }
+  
+    [System.Serializable]
+    public class Item
+    {
+
+        public Sprite Icon;
+        public string ItemName;
+        public string ItemInfo;
+        public float Amount;
+        public ItemType type;
+     
+    }
+    public enum ItemType
+    {
+        Weapon,Heal,resource,Gem,CharacterStar
+    }
+
+
+}
